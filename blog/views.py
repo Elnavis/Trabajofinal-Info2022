c2eeee83bff472a8bd7068698f881d32f4d3284c
tossuttid <tossuttid>
2022-12-16 00:04:46 -0300

    Carga de template

--- blog/views.py
+++ blog/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>Holis mundo!</h1>")
+    context = {}
+    return render(request, "blog/index.html", context)
