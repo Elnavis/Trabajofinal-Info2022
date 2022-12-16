93822442d3cc4fb01ceab0abfd2b5408777aa991
tossuttid <tossuttid>
2022-12-16 11:13:24 -0300

    Conexión exitosa mariadb

--- pagina/settings.py
+++ pagina/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'pagina.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "example",
+        'USER': "dev",
+        'PASSWORD': "1234",
+        'PORT': "3306"
     }
 }
 
